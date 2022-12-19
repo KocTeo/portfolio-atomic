3e2813ea402077b7f1a76568b5d88f5cb8fe6da9
Alexandre Koch Teodoreto <alekteodoreto@hotmail.com>
2022-12-19 19:44:47 -0300

    feat(Atoms): Creating ase files of Atoms

---
+++ src/components/Atoms/Icon.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+const Icon: React.FC = () => {
+  return <div />;
+};
+
+export default Icon;
---
+++ src/components/Atoms/SubTitle.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+const SubTitle: React.FC = () => {
+  return <div />;
+};
+
+export default SubTitle;
