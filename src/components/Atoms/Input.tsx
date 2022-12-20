cd1cc92cd2fa17a0f4e00784953c52f076a6a137
Alexandre Koch Teodoreto <alekteodoreto@hotmail.com>
2022-12-19 22:51:37 -0300

    feat(Atoms): Conditional Input
    Render a Input text or textarea

--- src/components/Atoms/Input.tsx
+++ src/components/Atoms/Input.tsx
@@ -2,19 +2,28 @@ import React from "react";
 
 interface Props {
   placeholder: string;
-  width: string;
+  type: string;
 }
 
-const Input: React.FC<Props> = ({ placeholder, width }) => {
-  return (
-    <input
-      type="text"
-      placeholder={placeholder}
-      className={
-        "mt-0 block w-1/3 px-0.5 border-0 border-b-2 focus:ring-0 focus:border-white bg-gray-900 text-gray-400"
-      }
-    />
-  );
+const Input: React.FC<Props> = ({ placeholder, type }) => {
+  {
+    return type === "text" ? (
+      <input
+        type="text"
+        placeholder={placeholder}
+        className={
+          "mt-0 block w-1/3 px-0.5 border-0 border-b-2 focus:ring-0 focus:border-white bg-gray-900 text-gray-400"
+        }
+      />
+    ) : (
+      <textarea
+        className={
+          "mt-0 block w-1/3 px-0.5 border-0 border-b-2 focus:ring-0 focus:border-white bg-gray-900 text-gray-400"
+        }
+        placeholder={placeholder}
+      />
+    );
+  }
 };
 
 export default Input;
