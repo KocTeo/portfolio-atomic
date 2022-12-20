4ac4ed7b486d8eaa6b3bf929497e8a1cbd003e49
Alexandre Koch Teodoreto <alekteodoreto@hotmail.com>
2022-12-20 18:42:38 -0300

    refactor(Atoms): Refactor responsive style for title and subtitle

--- src/components/Atoms/Title.tsx
+++ src/components/Atoms/Title.tsx
@@ -5,7 +5,9 @@ interface Props {
 }
 
 const Title: React.FC<Props> = ({ children }) => {
-  return <h1 className="text-5xl text-white font-bold">{children}</h1>;
+  return (
+    <h1 className="text-white font-bold text-4xl md:text-5xl">{children}</h1>
+  );
 };
 
 export default Title;
