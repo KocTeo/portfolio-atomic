1ca1b34b93fe1047ec72232d3231081328688b3c
Alexandre Koch Teodoreto <alekteodoreto@hotmail.com>
2022-12-19 22:46:25 -0300

    feat(Atoms): Input

--- src/components/Atoms/Input.tsx
+++ src/components/Atoms/Input.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 
-// import { Container } from './styles';
+interface Props {
+  placeholder: string;
+  width: string;
+}
 
-const Input: React.FC = () => {
-  return <div />;
+const Input: React.FC<Props> = ({ placeholder, width }) => {
+  return (
+    <input
+      type="text"
+      placeholder={placeholder}
+      className={
+        "mt-0 block w-1/3 px-0.5 border-0 border-b-2 focus:ring-0 focus:border-white bg-gray-900 text-gray-400"
+      }
+    />
+  );
 };
 
 export default Input;
