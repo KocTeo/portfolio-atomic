004355082c28623de63f65e1963f802f867ffb54
Alexandre Koch Teodoreto <alekteodoreto@hotmail.com>
2022-12-19 22:27:12 -0300

    feat(Atoms): Button

--- src/components/Atoms/Button.tsx
+++ src/components/Atoms/Button.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 
-// import { Container } from './styles';
+interface Props {
+  text: string;
+}
 
-const Button: React.FC = () => {
-  return <div />;
+const Button: React.FC<Props> = ({ text }) => {
+  return (
+    <button className="bg-gray-300 text-black font-bold py-2 px-4 border-b-4 border-gray-500 hover:bg-transparent hover:border-none hover:text-white rounded h-12 w-44 text-xl">
+      {text}
+    </button>
+  );
 };
 
 export default Button;
