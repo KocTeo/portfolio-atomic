3b962ba2351cefa0b8a67d221c8dc846ad4819c8
Alexandre Koch Teodoreto <alekteodoreto@hotmail.com>
2022-12-21 14:51:56 -0300

    feat(Molecules): Molecules About Text and Avatar from About Organism

--- src/components/Molecules/AboutText.tsx
+++ src/components/Molecules/AboutText.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import Title from "../Atoms/Title";
 
 // import { Container } from './styles';
 
 const AboutText: React.FC = () => {
-  return <div />;
+  return (
+    <div className="w-4/6">
+      <Title>Quem sou eu?</Title>
+      <p className="text-white text-2xl mt-8 text-justify">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur quis
+        ullam nobis blanditiis nesciunt provident ipsam distinctio repellat
+        commodi, vitae quidem quos illum magnam itaque sint debitis fugiat
+        voluptatum accusantium!
+      </p>
+      <p className="text-white text-2xl mt-8 text-justify">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur quis
+        ullam nobis blanditiis nesciunt provident ipsam distinctio repellat
+        commodi, vitae quidem quos illum magnam itaque sint debitis fugiat
+        voluptatum accusantium!
+      </p>
+    </div>
+  );
 };
 
 export default AboutText;
--- src/components/Molecules/Avatar.tsx
+++ src/components/Molecules/Avatar.tsx
@@ -3,7 +3,14 @@ import React from "react";
 // import { Container } from './styles';
 
 const Avatar: React.FC = () => {
-  return <div />;
+  return (
+    <>
+      <div
+        className="w-3/12 rounded-full bg-contain bg-no-repeat"
+        style={{ backgroundImage: 'url("/images/eu.jpeg")' }}
+      />
+    </>
+  );
 };
 
 export default Avatar;
