42dee995cff33eeab0293bae83e652c3a295ef8c
Alexandre Koch Teodoreto <alekteodoreto@hotmail.com>
2022-12-23 15:31:54 -0300

    refactor(Atoms): Add state and function onChange and onClick in Button and Input

--- src/components/Atoms/Button.tsx
+++ src/components/Atoms/Button.tsx
@@ -2,11 +2,15 @@ import React from "react";
 
 type Props = {
   children?: React.ReactNode;
+  onclick: (e: any) => void;
 };
 
-const Button: React.FC<Props> = ({ children }) => {
+const Button: React.FC<Props> = ({ children, onclick }) => {
   return (
-    <button className="bg-gray-300 text-black font-bold py-2 px-4  hover:bg-transparent hover:border-none hover:text-white rounded h-12 w-44 text-xl">
+    <button
+      onClick={onclick}
+      className="bg-gray-300 text-black font-bold py-2 px-4  hover:bg-transparent hover:border-none hover:text-white rounded h-12 w-44 text-xl"
+    >
       {children}
     </button>
   );
--- src/components/Atoms/Input.tsx
+++ src/components/Atoms/Input.tsx
@@ -3,24 +3,30 @@ import React from "react";
 interface Props {
   placeholder: string;
   type: string;
+  value?: string;
+  onchange?: (e: any) => void;
 }
 
-const Input: React.FC<Props> = ({ placeholder, type }) => {
+const Input: React.FC<Props> = ({ placeholder, type, value, onchange }) => {
   {
     return type === "text" ? (
       <input
         type="text"
         placeholder={placeholder}
         className={
-          "mt-0 block w-1/3 px-0.5 border-0 border-b-2 focus:ring-0 focus:border-white bg-gray-900 text-gray-400"
+          "mt-0 block w-5/12 px-0.5 border-0 border-b-2 focus:ring-0 focus:border-white bg-gray-900 text-gray-400 p-3 pl-2 text-lg"
         }
+        value={value}
+        onChange={onchange}
       />
     ) : (
       <textarea
         className={
-          "mt-0 block w-1/3 px-0.5 border-0 border-b-2 focus:ring-0 focus:border-white bg-gray-900 text-gray-400"
+          "mt-0 block w-full px-0.5 border-0 border-b-2 focus:ring-0 focus:border-white bg-gray-900 text-gray-400 p-3 pl-2"
         }
         placeholder={placeholder}
+        value={value}
+        onChange={onchange}
       />
     );
   }
